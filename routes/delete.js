16b85bfabdfee05d73965dc331660f433e1bf08d
lakhara <siddharth.lakhara@gmail.com>
2018-05-11 22:52:29 +0530

    delete operation

---
+++ routes/delete.js
@@ -0,0 +1,14 @@
+const Models = require('../models');
+
+module.exports = [{
+  method: 'POST',
+  path: '/delete',
+  handler: (req, reply) => {
+    const { userName } = req.payload;
+    Models.users.destroy({
+      where: { firstName: userName },
+    }).then(() => {
+      reply('User successfully deleted');
+    });
+  },
+}];
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const ping = require('./ping');
 const create = require('./create');
 const read = require('./read');
 const update = require('./update');
+const del = require('./delete');
 
 module.exports = [].concat(
   ping, create,
   read, update,
+  del,
 );
