4d8bea36785d1f896f5ff058734f71848aeadb8c
lakhara <siddharth.lakhara@gmail.com>
2018-05-11 22:54:49 +0530

    refactor: remove unused code

--- models/users.js
+++ models/users.js
@@ -1,14 +1,8 @@
-'use strict';
+
 
 module.exports = (sequelize, DataTypes) => {
-  var users = sequelize.define('users', {
-    firstName: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+  const users = sequelize.define('users', {
+    firstName: DataTypes.STRING,
   });
   return users;
 };
