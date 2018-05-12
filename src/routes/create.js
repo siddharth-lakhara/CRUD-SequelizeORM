043c2cccb0effe0fad4f580b970cb4008c778c34
lakhara <siddharth.lakhara@gmail.com>
2018-05-12 14:16:09 +0530

    tests added for create

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,13 +1,24 @@
 const Models = require('../../models');
+const Joi = require('joi');
 
 // Create new user
 module.exports = [{
   method: 'POST',
   path: '/users',
+  config: {
+    validate: {
+      payload: {
+        userName: Joi.string().required().error(new Error('userName field not found')),
+      },
+    },
+  },
   handler: (req, reply) => {
     const { userName } = req.payload;
-    Models.users.create({ firstName: userName })
-      .then(() => {
+    Models.users.findOrCreate({ where: { firstName: userName } })
+      .spread((_, created) => {
+        if (created) { reply('User successfully created'); }
+        reply('User already exists');
+      }).catch(() => {
         reply('User successfully created');
       });
   },
---
+++ tests/routes.test.js
@@ -0,0 +1,69 @@
+const Models = require('../models');
+const server = require('../src/server');
+
+beforeAll(() => {
+  Models.users.destroy({ where: {} });
+});
+
+describe('tests for create', () => {
+  test('create a new user', (done) => {
+    const options = {
+      url: '/users',
+      method: 'POST',
+      payload: JSON.stringify({ userName: 'testUser' }),
+    };
+    server.inject(options).then((res) => {
+      expect(res.payload).toEqual('User successfully created');
+      done();
+    });
+  });
+
+  test('rejects a request if userName is not provided', (done) => {
+    const options = {
+      url: '/users',
+      method: 'POST',
+      payload: JSON.stringify({}),
+    };
+    server.inject(options).then((response) => {
+      payload = JSON.parse(response.payload);
+      // console.log(payload);
+      expect(payload.statusCode).toBe(400);
+      expect(payload.message).toEqual('userName field not found');
+      done();
+    });
+  });
+
+  test('Does not add an existing user again', (done) => {
+    const options = {
+      url: '/users',
+      method: 'POST',
+      payload: JSON.stringify({ userName: 'testUser' }),
+    };
+    server.inject(options).then((res) => {
+      expect(res.payload).toEqual('User already exists');
+      done();
+    });
+  });
+});
+
+// describe('test for read', () => {
+//   test('Reads all users successfully');
+// });
+
+// describe('test for update', () => {
+//   test('Updates a user already in DB');
+
+//   test('Returns error if user is not found');
+
+//   test('rejects request if old userName is not provided');
+
+//   test('rejects request if new userName is not provided');
+// });
+
+// describe('test for delete operation', () => {
+//   test('Delets a user present in DB');
+
+//   test('Returns error if user is not found');
+
+//   test('rejects a request if userName is not provided');
+// });
